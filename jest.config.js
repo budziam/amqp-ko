e38182b2a4405c33913a7f434fb93de1752a0eb5
Michał Budziak <budziam@users.noreply.github.com>
2019-07-19 20:22:24 +0200

    Use linter (#6)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    globals: {
+        "ts-jest": {
+            tsConfig: "tsconfig.test.json"
+        }
+    },
     preset: "ts-jest",
     reporters: ["default", "jest-junit"],
     setupFilesAfterEnv: ['<rootDir>/__tests__/setup.ts'],
